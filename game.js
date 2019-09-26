e9ffddbafde332deb192cfcd41cd906631296406
JDXue <jessica.dam@yahoo.com>
2019-09-26 17:38:26 +0100

    Movement functions have been moved into the player and rock variables

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 
 // Frank Poth 08/13/2017
 
-var context, controller, player, loop, dropRock, rock;
+var context, controller, player, loop, rock;
 var stageheight = 300;
 var stagewidth = 540;
 
@@ -19,7 +19,64 @@ player = {
   x:stagewidth/2, // center of the canvas
   x_velocity:0,
   y:0,
-  y_velocity:0
+  y_velocity:0,
+
+  //function for movement is now a method of the player class
+  playerMove: function(){
+    if (controller.up && player.jumping == false) {
+
+      player.y_velocity -= 28; //max height of player jump
+      player.jumping = true;
+
+    }
+
+    if (controller.left) {
+
+      player.x_velocity -= 0.5;
+
+    }
+
+    if (controller.right) {
+
+      player.x_velocity += 0.5;
+
+    }
+
+    player.y_velocity += 0.5;// gravity
+    player.x += player.x_velocity;
+    player.y += player.y_velocity;
+    player.x_velocity *= 0.9;// friction
+    player.y_velocity *= 0.8;// friction
+
+    // if player is falling below floor line
+    if (player.y > stageheight - 5 - 32) {
+
+      player.jumping = false;
+      player.y = stageheight - 5 - 32;
+      player.y_velocity = 0;
+
+    }
+
+    // if player is going off the left of the screen
+    if (player.x < -32) {
+
+      player.x = stagewidth;
+
+    }
+    else if (player.x > stagewidth) {// if player goes past right boundary
+
+      player.x = -32;
+
+    }
+
+    //draw player
+    context.fillStyle = "#fff";// hex for white
+    context.beginPath();
+    context.arc(player.x, player.y, 15, player.width, player.height*Math.PI,false);
+    context.fill();
+
+
+  }
 
 };
 
@@ -57,96 +114,43 @@ rock = {
   x: stagewidth, // right hand side of the canvas
   x_velocity:0,
   y:0,
-  y_velocity:0
+  y_velocity:0,
+
+  //rock release function is now a method of the rock class
+  rockDrop:function(){
+    rock.x_velocity -= 0.5;
+    rock.y_velocity += 0.5;
+    rock.x += rock.x_velocity;
+
+    rock.y_velocity += 0.7;// gravity
+    rock.x += rock.x_velocity;
+    rock.y += rock.y_velocity;
+    rock.x_velocity *= 0.7;// friction
+    rock.y_velocity *= 0.8;// friction
+
+    if (rock.y > stageheight - 5 - 32){
+      rock.falling = false;
+      rock.y = stageheight - 5 - 32;
+      rock.y_velocity = 0;
+    }
+
+    if (rock.x < - 32){
+      rock.x = stagewidth + 5;
+      rock.y = 0;
+    }
+
+    console.log("dropping a rock");
+    context.fillStyle = '#f0f';
+    context.beginPath();
+    context.arc(rock.x, rock.y, 15, rock.width, rock.height*Math.PI,false);
+    context.fill();
+  }
 };
 
-function playerMove(){
-  if (controller.up && player.jumping == false) {
-
-    player.y_velocity -= 28; //max height of player jump
-    player.jumping = true;
-
-  }
-
-  if (controller.left) {
-
-    player.x_velocity -= 0.5;
-
-  }
-
-  if (controller.right) {
-
-    player.x_velocity += 0.5;
-
-  }
-
-  player.y_velocity += 0.5;// gravity
-  player.x += player.x_velocity;
-  player.y += player.y_velocity;
-  player.x_velocity *= 0.9;// friction
-  player.y_velocity *= 0.8;// friction
-
-  // if player is falling below floor line
-  if (player.y > stageheight - 5 - 32) {
-
-    player.jumping = false;
-    player.y = stageheight - 5 - 32;
-    player.y_velocity = 0;
-
-  }
-
-  // if player is going off the left of the screen
-  if (player.x < -32) {
-
-    player.x = stagewidth;
-
-  } else if (player.x > stagewidth) {// if player goes past right boundary
-
-    player.x = -32;
-
-  }
-
-  //draw player
-  context.fillStyle = "#fff";// hex for white
-  context.beginPath();
-  context.arc(player.x, player.y, 15, player.width, player.height*Math.PI,false);
-  context.fill();
 
 
-};
 
-function rockDrop(){
-  rock.x_velocity -= 0.5;
-  rock.y_velocity += 0.5;
-  rock.x += rock.x_velocity;
 
-  rock.y_velocity += 0.7;// gravity
-  rock.x += rock.x_velocity;
-  rock.y += rock.y_velocity;
-  rock.x_velocity *= 0.7;// friction
-  rock.y_velocity *= 0.8;// friction
-
-  if (rock.y > stageheight - 5 - 32){
-    rock.falling = false;
-    rock.y = stageheight - 5 - 32;
-    rock.y_velocity = 0;
-  }
-
-  if (rock.x < - 32){
-    rock.x = stagewidth + 5;
-    rock.y = 0;
-  }
-
-  console.log("dropping a rock");
-  context.fillStyle = '#f0f';
-  context.beginPath();
-  context.arc(rock.x, rock.y, 15, rock.width, rock.height*Math.PI,false);
-  context.fill();
-}
-
-if(controller.up || controller.right || controller.left){
-  rocks = true;
-}
 //function is reiterated for the duration of the script runtime
 loop = function() {
   //draw stage
@@ -159,8 +163,8 @@ loop = function() {
   context.lineTo(stagewidth, 284);
   context.stroke();
 
-  playerMove();
-  rockDrop();
+  player.playerMove();
+  rock.rockDrop();
 
   // call update when the browser is ready to draw again
   window.requestAnimationFrame(loop);
